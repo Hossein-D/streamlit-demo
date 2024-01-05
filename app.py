c621c4b3d9b595f7958e6cd0a7afa78aed8919de
Hossein Dehghani <d.hossein.d@gmail.com>
2024-01-05 15:45:26 -0500

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,49 @@
 """Small example streamlit application."""
 import streamlit as st
+st.title("hello world")
 
+# set the app's title                                                   
+st.title("Text Elements")
+
+# header                                                                
+st.header("Header in Streamlit")
+
+# subheader                                                             
+st.subheader("Subheader in Streamlit")
+
+# markdown                                                              
+# display text in bold formatting                                       
+st.markdown("**AskPython** is an awesome website!")
+# display text in italic formatting                                     
+st.markdown("Visit _askpython.com_ to learn from various Python tutoria\
+ls.")
+
+# code block                                                            
+code = '''                                                              
+def add(a, b):                                                          
+    print("a+b = ", a+b)                                                
+'''
+st.code(code, language='python')
+
+# latex                                                                 
+st.latex('''                                                            
+(a+b)^2 = a^2 + b^2 + 2*a*b                                             
+''')
+
+#button                                                                 
+if st.button('Click here', help="Click to see the text change"):
+    st.write('Hi there!')
+else:
+    st.write('Goodbye')
+
+# check box                                                             
+checked = st.checkbox('Click here')
+if checked:
+    st.write('Good job!')
+
+
+
+"""
 def app():
     """Define main application entry point."""
     st.title("Minimal Streamlit Template")
@@ -19,3 +62,5 @@ you may want to consider
 
 if __name__ == '__main__':
     app()
+"""
+
